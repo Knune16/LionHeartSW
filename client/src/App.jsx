e76c1e342ff251632aff51773feeb7e256938314
Keylor <98989024+Knune16@users.noreply.github.com>
2025-01-07 21:09:16 -0600

    Add layout for pages components + admin view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import AuthLayout from './components/auth/layout'
+import AuthLogin from './pages/auth/login'
+import AuthRegister from './pages/auth/register'
+import AdminLayout from './components/admin/layout'
+import AdminDashboard from './pages/admin/dashboard'
+import AdminProducts from './pages/admin/products'
+import AdminOrders from './pages/admin/orders'
+import AdminFeatures from './pages/admin/features'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className="flex flex-col overflow-hidden bg-white">
+    {/*common component */}
+    <h1>Header Component</h1>
+
+    <Routes>
+      <Route path='/auth' element={<AuthLayout/>}>
+        <Route path='login' element={<AuthLogin/>}/>
+        <Route path='register' element={<AuthRegister/>}/>
+      </Route>
+      <Route path='/admin' element={<AdminLayout/>}>
+        <Route path='dashboard'element={<AdminDashboard/>} />
+        <Route path='products' element={<AdminProducts/>}/>
+        <Route path='orders' element={<AdminOrders/>} />
+        <Route path='features' element={<AdminFeatures/>} />
+
+      </Route>
+    </Routes>
+   </div>
   )
 }
 
---
+++ client/src/pages/admin/dashboard.jsx
@@ -0,0 +1,9 @@
+
+
+function AdminDashboard() {
+    return (
+        <div>Admin DashBoard</div>
+      );
+}
+
+export default AdminDashboard;
